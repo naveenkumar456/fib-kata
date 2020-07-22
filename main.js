d5164467beb3cc280e0316ad3c11f250eb32dbb5
naveenkumar456 <68639076+naveenkumar456@users.noreply.github.com>
2020-07-22 18:24:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function main(num) {
         if(num < 0) throw new Error('num cannot be negative number')  
         else if(num === 0) return 0;
-        return (num === 1 || num === 2) ? num-1 : main(num-1) + main(num-2)
+        else return (num === 1 || num === 2) ? num-1 : main(num-1) + main(num-2)
     }
 }
 
